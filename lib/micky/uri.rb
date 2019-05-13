rafbm/micky

commit 631eff9ca432b004cadb39a0d5b2d8cb204a38f7

Don’t parse "http://" as a valid URI

lib/micky/uri.rb
@@ -11,7 +11,7 @@ module Micky
     else
       uri = "http://#{uri}"
     end
-    begin
+    uri = begin
       ::URI.parse(uri)
     rescue ::URI::InvalidURIError
       begin
@@ -19,6 +19,7 @@ module Micky
       rescue ::URI::InvalidURIError
       end
     end
+    uri if uri.host
   end
 
   module URI

commit bc89292848a03e83b26033be318c7ce4294aca4f

Fix regressions introduced in 631eff9

lib/micky/uri.rb
@@ -9,6 +9,7 @@ module Micky
       # Replace any number of slashes (1, 3 or 4579) by two slashes
       uri.sub! %r{/+}, '//'.freeze
     else
+      uri.sub! %r{/+}, ''.freeze
       uri = "http://#{uri}"
     end
     uri = begin
@@ -19,7 +20,7 @@ module Micky
       rescue ::URI::InvalidURIError
       end
     end
-    uri if uri.host
+    uri if uri&.host
   end
 
   module URI